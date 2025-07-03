d8d38e1aac1fe7590a47228d5ae119dbef7fbbbd
sun-chingwoo <sunchingwoo@gmail.com>
2025-07-04 00:38:27 +0530

    planned for backtracking and set up filtering and prepared checkinput

--- 1st model.py
+++ 1st model.py
@@ -6,24 +6,33 @@ def srows():
     for i in range(0,9):
         row=[board[i][j] for j in range (9)]
         rows.append(row)
-    print(rows)
-
+    return(rows)
 def scolumns():
     columns=[]
     for j in range(0,9):
         column=[board[i][j] for i in range(9)]
         columns.append(column)
-    print(columns)
-
-def sbox():
+    return(columns)
+def sboxes():
     boxes=[]
     for m in range (0,9,3):
         for k in range(0,9,3):
             box=[board[i][j] for j in range(k,k+3) for i in range (m,m+3)]
             boxes.append(box)
-    print(boxes)
-
-
+    return(boxes)
+def end():
+    ideal=[1,2,3,4,5,6,7,8,9]
+    rows=srows()
+    columns=scolumns()
+    boxes=sboxes()
+    for i in range(0,9):
+        if rows[i]!=ideal:
+            return(False)
+        if columns[i]!=ideal:
+            return(False)
+        if boxes[i]!=ideal:
+            return(False)
+    return True
 def dis():
     for i in range(0,9):
         for j in range(0,9):
@@ -35,6 +44,16 @@ def dis():
             print("_"*40)
             print()
 
+
+def filter(a):
+    filtered = []
+    for i in range(9):
+        temp = [x for x in a[i] if x != " "]
+        filtered.append(temp)
+    return filtered
+
+
+
 dis()
 n=int(input("enter number of known places"))
 for i in range (n):
@@ -44,7 +63,24 @@ for i in range (n):
     board[irow-1][icol-1]=value
 
 dis()
-sbox()
+p=filter(sboxes())
+print(p)
 
 
+
+
+
+def checkinput():
+    cideal={1,2,3,4,5,6,7,8,9}
+    rows=srows()
+    columns=scolumns()
+    boxes=sboxes()
+    for i in range(0,9): 
+        if not rows[i] <= cideal:
+            return(False)
+        if not columns[i] <= cideal:
+            return(False)
+        if not boxes[i] <= cideal:
+            return(False)
+    return True
 
